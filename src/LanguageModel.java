c5420ca34b44486f4378f24f169bf8cb60dbc678
lkas96 <kwek_lawson@hotmail.com>
2024-10-08 16:18:19 +0800

    More methods examples randomise

--- src/LanguageModel.java
+++ src/LanguageModel.java
@@ -1,11 +1,13 @@
 package src;
 import java.io.*;
+import java.security.SecureRandom;
 import java.util.*;
 
 public class LanguageModel {
 
     private Reader reader;
     private Map<String, Map<String, Integer>> nextWords;
+    private Random rand = new SecureRandom();
 
     public LanguageModel(Reader reader) {
         this.reader = reader;
@@ -73,5 +75,100 @@ public class LanguageModel {
                 System.out.printf("\t\t%s = %d\n", next, dist.get(next));
         }
         System.out.println("---------------------------------------------");
+    }
+
+    //Look through the keyset, to find the next word.
+    //Give the first word, find the highest count next word.
+    public String nextWord(String word){
+        int wordCount = -1;
+        String theWord = "";
+
+        Map<String, Integer> dist = nextWords.get(word);
+        if (null == dist){
+            return "";
+        }
+
+        for (String w: dist.keySet()){
+            if (dist.get(w) > wordCount){
+                wordCount = dist.get(w);
+                theWord = w;
+            }
+        }
+        return theWord;
+    }
+
+    //METHOD TO GENERATE SENTENCE, TAKES IN A STARTING WORD AND SENTENCE LIMIT. 
+    public String generate(String startingWord, int numberOfWords){
+
+        String theSentence = startingWord;
+
+        String curr = startingWord;
+        String next = "";
+
+        for (int i = 0; i < numberOfWords; i++){
+            //Take in the first words
+            //generate the whole paragraph and capped at number of words given. 
+            // next = nextWord(curr);
+            next = randomNext(curr);
+            if(next.length() <= 0){
+                return theSentence + ".";
+            }
+            theSentence += " " + next; //add on the next word to the sentence
+            
+            //continutes loop, the next word is now the first word, continue looking for next word. 
+        }
+
+        return theSentence + ".";
+    }
+
+    public String generate2(String startingWord, int numberOfWords, float hallucination){
+        String theSentence = startingWord;
+        
+        String curr = startingWord;
+        String next = "";
+
+        float totalRandom = 1 - hallucination;
+
+        for (int i = 0; i < numberOfWords; i++){
+
+            if (rand.nextFloat() > totalRandom){
+                next = nextWord(curr); //Exploration
+            } else {
+                next = randomNext(curr);
+            }
+            //Take in the first words
+            //generate the whole paragraph and capped at number of words given. 
+            // next = nextWord(curr);
+            
+            if(next.length() <= 0){
+                return theSentence + ".";
+            }
+            theSentence += " " + next; //add on the next word to the sentence
+            
+            //continutes loop, the next word is now the first word, continue looking for next word. 
+        }
+
+        return theSentence + ".";
+    }
+
+    //Exploration
+    public String randomNext(String word){
+        Map<String, Integer> dist = nextWords.get(word);
+        if (null == dist){
+            return "";
+        }
+        int numWords = dist.size();
+        //Randomly picka  number from the lsit of next words
+        int theWord = rand.nextInt(numWords);
+        int i = 0;
+        String nw = "";
+        for (String w: nextWords.keySet()){
+            nw = w;
+            if ( i > theWord){
+                return w;
+            }
+            i++;
+        }
+        return nw;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,19 @@ public class Main {
         FileReader r = new FileReader(args[0]);
         LanguageModel lm = new LanguageModel(r);
         lm.buildModel();
-        lm.dumpModel();
+        // lm.dumpModel();
 
+        Console cons = System.console();
+        while(true){
+            String startingWord = cons.readLine(">> Starting word: ");
+            String numberOfWords = cons.readLine(">> Number of words: ");
+
+            int numWords = Integer.parseInt(numberOfWords);
+
+            String generated = lm.generate2(startingWord, numWords, 0.1f);
+
+            System.out.printf("======================================\n%s\n", generated);
+        }
 
 
 
